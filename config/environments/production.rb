800ca1613e1a784973b433b12c299ec1a321df66
“innocent <rockcoolsaint@gmail.com>
2017-04-24 00:04:44 +0100

    port 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
-  :port                 => 465,
+  :port                 => 587,
   :user_name            => ENV['gmail_username'],
   :password             => ENV['gmail_password'],
   :authentication       => "plain",
