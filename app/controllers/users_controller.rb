a3a83ad75b6bae2707f120bebe76f39a9acbec38
“innocent <rockcoolsaint@gmail.com>
2017-04-23 23:08:39 +0100

    mail delivery enabled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         # Sends email to user when user is created.
-        #ExampleMailer.sample_email(@user).deliver_now
+        ExampleMailer.sample_email(@user).deliver_now
 
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
