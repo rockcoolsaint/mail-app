c745e24d4270376dfb94de47542aaa49cac1ef32
“innocent <rockcoolsaint@gmail.com>
2017-05-01 21:18:53 +0100

    heroku config vars used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.mailgun.org",
   :port                 => 587,
-  :user_name            => 'postmaster@sandbox0b131cf789f24016bc84c836a95b9a7b.mailgun.org',
-  :password             => '85ad286bf086d2b59413bb3bd64dfaaa',
+  :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
+  :password             => ENV['MAILGUN_SMTP_PASSWORD'],
   :authentication       => "plain",
-  :domain         => 'sandbox0b131cf789f24016bc84c836a95b9a7b.mailgun.org',
+  :domain         => ENV['MAILGUN_DOMAIN'],
  :enable_starttls_auto => true
  }
 end
